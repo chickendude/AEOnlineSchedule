f7d4c83dbaff0aee3f54340b820036225daa6377
chickendude <pokemaster103@gmail.com>
2017-10-27 23:18:03 +0200

    Added dialog to prompt whether to add default class types if none found. Also added "add new class type" button at bottom.

--- app/src/main/java/ch/ralena/aeonlineschedule/adapters/NewClassTypeAdapter.java
+++ app/src/main/java/ch/ralena/aeonlineschedule/adapters/NewClassTypeAdapter.java
@@ -16,7 +16,7 @@ import ch.ralena.aeonlineschedule.objects.ClassType;
  * Recycler View adapter for new class types.
  */
 // TODO: 10/27/2017 Add extra row at the bottom for a "new class type"
-public class NewClassTypeAdapter extends RecyclerView.Adapter<NewClassTypeAdapter.NCTViewHolder>{
+public class NewClassTypeAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 	private List<ClassType> classTypes;
 
 	public NewClassTypeAdapter(List<ClassType> classTypes) {
@@ -24,20 +24,36 @@ public class NewClassTypeAdapter extends RecyclerView.Adapter<NewClassTypeAdapte
 	}
 
 	@Override
-	public NCTViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-		View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_new_class_type, parent, false);
-		return new NCTViewHolder(view);
+	public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+		if (viewType==0) {
+			View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_new_class_type, parent, false);
+			return new NCTViewHolder(view);
+		} else {
+			View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_new_class_type_end_button, parent, false);
+			return new RecyclerView.ViewHolder(view) {};
+		}
 	}
 
 	@Override
-	public void onBindViewHolder(NCTViewHolder holder, int position) {
-		ClassType classType = classTypes.get(position);
-		holder.bindView(classType);
+	public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+		if (position < classTypes.size()) {
+			ClassType classType = classTypes.get(position);
+			((NCTViewHolder) holder).bindView(classType);
+		}
+	}
+
+	@Override
+	public int getItemViewType(int position) {
+		if (position < classTypes.size()) {
+			return 0;
+		} else {
+			return 1;
+		}
 	}
 
 	@Override
 	public int getItemCount() {
-		return classTypes.size();
+		return classTypes.size() + 1;
 	}
 
 	class NCTViewHolder extends RecyclerView.ViewHolder {
--- app/src/main/java/ch/ralena/aeonlineschedule/fragments/ClassTypeFragment.java
+++ app/src/main/java/ch/ralena/aeonlineschedule/fragments/ClassTypeFragment.java
@@ -1,5 +1,6 @@
 package ch.ralena.aeonlineschedule.fragments;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -9,13 +10,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import ch.ralena.aeonlineschedule.R;
 import ch.ralena.aeonlineschedule.adapters.NewClassTypeAdapter;
 import ch.ralena.aeonlineschedule.objects.ClassType;
+import io.realm.Realm;
 
 /**
  * Fragment for viewing and editing/creating class types.
@@ -24,10 +24,13 @@ import ch.ralena.aeonlineschedule.objects.ClassType;
 public class ClassTypeFragment extends Fragment {
 	RecyclerView classTypeRecyclerView;
 	RecyclerView newClassTypeRecyclerView;
+	Realm realm;
+	List<ClassType> classTypes;
 
 	@Nullable
 	@Override
 	public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+		realm = Realm.getDefaultInstance();
 		View view = inflater.inflate(R.layout.fragment_class_type, container, false);
 
 		classTypeRecyclerView = view.findViewById(R.id.classTypeRecyclerView);
@@ -37,31 +40,59 @@ public class ClassTypeFragment extends Fragment {
 		return view;
 	}
 
+	/**
+	 * Sets up the recycler view, loads the class types, and checks whether to load the default class types.
+	 */
 	private void setUpNewClassRecyclerView() {
-		List<ClassType> classTypes = new ArrayList<>();
-		// classes that take 40 minutes
-		String[] classType40Names = {"GES", "GE", "BE", "IELTS", "TOEFL", "SAT", "ACT", "EENG",
-				"MKTADV", "FIN", "HR", "IT", "Demo"};
-		for (String classTypeName : classType40Names) {
-			ClassType classType = new ClassType();
-			classType.setName(classTypeName);
-			classType.setNumMinutes(40);
-			classType.setWage(15f);
-			classTypes.add(classType);
+		classTypes = realm.where(ClassType.class).findAll().sort("name");
+		if (classTypes.size() == 0) {
+			askToLoadDefaultClassTypes();
 		}
-		// classes that take 45 minutes
-		String[] classType45Names = {"Group"};
-		for (String classTypeName : classType45Names) {
-			ClassType classType = new ClassType();
-			classType.setName(classTypeName);
-			classType.setNumMinutes(40);
-			classType.setWage(15f);
-			classTypes.add(classType);
-		}
-		Collections.sort(classTypes, (cT1, cT2) -> cT1.getName().compareTo(cT2.getName()));
 		// create adapter and set layout manager
 		NewClassTypeAdapter adapter = new NewClassTypeAdapter(classTypes);
 		classTypeRecyclerView.setAdapter(adapter);
 		classTypeRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+	}
+
+
+	/**
+	 * Loads an Alert Dialog box asking user if they want to load the default class types.
+	 * If yes, loads default class types, if no, does nothing.
+	 */
+	private void askToLoadDefaultClassTypes() {
+		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+		builder.setTitle("No Class Types found!");
+		builder.setMessage("Would you like to load the default class types?");
+		builder.setPositiveButton(android.R.string.yes, (dialogInterface, i) -> {
+			loadDefaultClassTypes();
+		});
+		builder.setNegativeButton(android.R.string.cancel, null);
+		builder.show();
+	}
+
+	/**
+	 * Creates the default class types and saves them in the realm db.
+	 */
+	private void loadDefaultClassTypes() {
+		String[] classType40Names = {"GES", "GE", "BE", "IELTS", "TOEFL", "SAT", "ACT", "EENG",
+				"MKTADV", "FIN", "HR", "IT", "Demo"};
+		String[] classType45Names = {"Group"};
+		realm.executeTransaction(realm -> {
+			// classes that take 40 minutes
+			for (String classTypeName : classType40Names) {
+				ClassType classType = realm.createObject(ClassType.class);
+				classType.setName(classTypeName);
+				classType.setNumMinutes(40);
+				classType.setWage(15f);        // todo: Load default wage value from options
+			}
+			// classes that take 45 minutes
+			for (String classTypeName : classType45Names) {
+				ClassType classType = realm.createObject(ClassType.class);
+				classType.setName(classTypeName);
+				classType.setNumMinutes(40);
+				classType.setWage(15f);        // todo: Load default wage value from options
+			}
+		});
+//		classTypes = realm.where(ClassType.class).findAll();
 	}
 }
