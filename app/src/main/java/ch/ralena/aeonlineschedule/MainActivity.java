8a74fef19b42e7de44f5f650bf744d11cce6436e
chickendude <pokemaster103@gmail.com>
2017-11-29 13:21:22 -0500

    Removed some redundant code from MainActivity.java.

--- app/src/main/java/ch/ralena/aeonlineschedule/MainActivity.java
+++ app/src/main/java/ch/ralena/aeonlineschedule/MainActivity.java
@@ -1,11 +1,9 @@
 package ch.ralena.aeonlineschedule;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
 
 import ch.ralena.aeonlineschedule.fragments.ScheduleFragment;
 
@@ -16,30 +14,27 @@ public class MainActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		BottomNavigationView navigationView = (BottomNavigationView) findViewById(R.id.navigationView);
+		BottomNavigationView navigationView = findViewById(R.id.navigationView);
 
 		// load proper fragment when menu item is pressed
-		navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-			@Override
-			public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-				Fragment fragment = null;
-				// check which menu item was pressed and load fragment object
-				switch (item.getItemId()) {
-					case R.id.menu_schedule:
-						fragment = new ScheduleFragment();
-						break;
-					case R.id.menu_invoice:
-						fragment = new ScheduleFragment();
-						break;
-				}
-				// if a proper press was registered, load that fragment
-				if (fragment != null) {
-					getSupportFragmentManager().beginTransaction()
-							.replace(R.id.fragmentContainer, fragment)
-							.commit();
-				}
-				return true;
+		navigationView.setOnNavigationItemSelectedListener(item -> {
+			Fragment fragment = null;
+			// check which menu item was pressed and load fragment object
+			switch (item.getItemId()) {
+				case R.id.menu_schedule:
+					fragment = new ScheduleFragment();
+					break;
+				case R.id.menu_invoice:
+					fragment = new ScheduleFragment();
+					break;
 			}
+			// if a proper press was registered, load that fragment
+			if (fragment != null) {
+				getSupportFragmentManager().beginTransaction()
+						.replace(R.id.fragmentContainer, fragment)
+						.commit();
+			}
+			return true;
 		}); // end of item selected listener
 		navigationView.setSelectedItemId(R.id.menu_schedule);
 	}
