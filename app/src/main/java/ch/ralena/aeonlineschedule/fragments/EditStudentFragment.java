6c5555114aa99bddd15fb52ccb342f2b915cb485
chickendude <pokemaster103@gmail.com>
2017-12-04 16:21:57 -0500

    Up button on student edit fragment works.

--- app/src/main/java/ch/ralena/aeonlineschedule/fragments/EditStudentFragment.java
+++ app/src/main/java/ch/ralena/aeonlineschedule/fragments/EditStudentFragment.java
@@ -3,30 +3,44 @@ package ch.ralena.aeonlineschedule.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import ch.ralena.aeonlineschedule.R;
+import ch.ralena.aeonlineschedule.objects.Student;
 import io.realm.Realm;
 
 public class EditStudentFragment extends Fragment {
 	public static final String EXTRA_STUDENT_ID = "extra_student_id";
 
+	ActionBar actionBar;
+
 	Realm realm;
+	Student student;
+
 	@Nullable
 	@Override
 	public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-		((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+		setHasOptionsMenu(true);
+		actionBar = ((AppCompatActivity) getActivity()).getSupportActionBar();
+		if (actionBar != null)
+			actionBar.setDisplayHomeAsUpEnabled(true);
 		realm = Realm.getDefaultInstance();
+
+		String studentId = getArguments().getString(EXTRA_STUDENT_ID);
+		student = realm.where(Student.class).equalTo("id", studentId).findFirst();
 
 		View view = inflater.inflate(R.layout.fragment_edit_student, container, false);
 
-		setHasOptionsMenu(true);
-
+		EditText studentNameEdit = view.findViewById(R.id.studentNameEdit);
+		studentNameEdit.setText(student.getName());
 
 		return view;
 	}
@@ -37,8 +51,19 @@ public class EditStudentFragment extends Fragment {
 	}
 
 	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch(item.getItemId()) {
+			case android.R.id.home:
+				getFragmentManager().popBackStackImmediate();
+				break;
+		}
+		return true;
+	}
+
+	@Override
 	public void onStop() {
 		super.onStop();
-		((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+		if (actionBar != null)
+			actionBar.setDisplayHomeAsUpEnabled(false);
 	}
 }
