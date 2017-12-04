b0eab7311acdbb7b624f70fc622c00c51ad0bf72
chickendude <pokemaster103@gmail.com>
2017-12-04 17:39:14 -0500

    Show month name in my classes fragment.

--- app/src/main/java/ch/ralena/aeonlineschedule/adapters/MyClassesAdapter.java
+++ app/src/main/java/ch/ralena/aeonlineschedule/adapters/MyClassesAdapter.java
@@ -6,14 +6,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.util.Calendar;
 import java.util.List;
 
 import ch.ralena.aeonlineschedule.R;
 import ch.ralena.aeonlineschedule.objects.ScheduledClass;
 
 public class MyClassesAdapter extends AEAdapter<ScheduledClass> {
-
-	List<ScheduledClass> scheduledClasses;
 
 	public MyClassesAdapter(List<ScheduledClass> scheduledClasses) {
 		super(scheduledClasses);
@@ -23,6 +22,7 @@ public class MyClassesAdapter extends AEAdapter<ScheduledClass> {
 	public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 		View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_my_classes, parent, false);
 		return new AEViewHolder(view) {
+			TextView monthName;
 			TextView studentName;
 			TextView time;
 			TextView date;
@@ -30,6 +30,8 @@ public class MyClassesAdapter extends AEAdapter<ScheduledClass> {
 
 			@Override
 			void loadViews() {
+				monthName = itemView.findViewById(R.id.monthNameText);
+				monthName.setVisibility(View.GONE);
 				studentName = itemView.findViewById(R.id.studentNameText);
 				time = itemView.findViewById(R.id.timeText);
 				date = itemView.findViewById(R.id.dateText);
@@ -38,6 +40,23 @@ public class MyClassesAdapter extends AEAdapter<ScheduledClass> {
 
 			@Override
 			void bindView(ScheduledClass scheduledClass) {
+				int position = objectsList.indexOf(scheduledClass);
+				if (position == 0) {
+					Calendar calendar = Calendar.getInstance();
+					calendar.setTime(scheduledClass.getDate());
+					monthName.setText(scheduledClass.getMonth());
+					monthName.setVisibility(View.VISIBLE);
+				}
+				if (position > 0) {
+					Calendar prevCalendar = Calendar.getInstance();
+					prevCalendar.setTime(objectsList.get(position - 1).getDate());
+					Calendar calendar = Calendar.getInstance();
+					calendar.setTime(scheduledClass.getDate());
+					if (calendar.get(Calendar.MONTH) != prevCalendar.get(Calendar.MONTH)) {
+						monthName.setText(scheduledClass.getMonth());
+						monthName.setVisibility(View.VISIBLE);
+					}
+				}
 				studentName.setText(scheduledClass.getStudent().getName());
 				date.setText(scheduledClass.getDayOfMonth());
 				dayOfWeek.setText(scheduledClass.getDayOfWeek());
