3b6b9b0e603ba614e180c11e24c2d71aafd559e5
chickendude <pokemaster103@gmail.com>
2017-12-04 11:00:03 -0500

    Filtering students in studentselectfragment works.

--- app/src/main/java/ch/ralena/aeonlineschedule/adapters/StudentSelectAdapter.java
+++ app/src/main/java/ch/ralena/aeonlineschedule/adapters/StudentSelectAdapter.java
@@ -10,10 +10,27 @@ import java.util.List;
 
 import ch.ralena.aeonlineschedule.R;
 import ch.ralena.aeonlineschedule.objects.Student;
+import io.reactivex.subjects.PublishSubject;
 
 public class StudentSelectAdapter extends AEAdapter<Student> {
+	PublishSubject<Student> studentObservable = PublishSubject.create();
+	PublishSubject<View> buttonObservable = PublishSubject.create();
+
+	public PublishSubject<Student> asObservableStudent() {
+		return studentObservable;
+	}
+
+	public PublishSubject<View> asObservableButton() {
+		return buttonObservable;
+	}
+
 	public StudentSelectAdapter(List<Student> studentList) {
 		super(studentList);
+	}
+
+	public void updateStudents(List<Student> studentList) {
+		objectsList = studentList;
+		notifyDataSetChanged();
 	}
 
 	@Override
@@ -29,6 +46,7 @@ public class StudentSelectAdapter extends AEAdapter<Student> {
 	@Override
 	public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 		if(viewType == TYPE_NORMAL) {
+			// students
 			View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_student, parent, false);
 			return new AEViewHolder(view) {
 				TextView studentName;
@@ -41,15 +59,18 @@ public class StudentSelectAdapter extends AEAdapter<Student> {
 				@Override
 				void bindView(Student student) {
 					studentName.setText(student.getName());
+					view.setOnClickListener(v -> studentObservable.onNext(student));
 				}
 			};
 		} else {
+			// create student button
 			View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_student_end, parent, false);
 			return new AEViewHolderBottom(view) {
 				TextView studentName;
 
 				@Override
 				void loadViews() {
+					view.setOnClickListener(v -> buttonObservable.onNext(v));
 					studentName = view.findViewById(R.id.studentNameText);
 					studentName.setText("Create New Student");
 				}
--- app/src/main/java/ch/ralena/aeonlineschedule/fragments/StudentSelectFragment.java
+++ app/src/main/java/ch/ralena/aeonlineschedule/fragments/StudentSelectFragment.java
@@ -6,20 +6,27 @@ import android.support.v4.app.Fragment;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.List;
 
 import ch.ralena.aeonlineschedule.R;
 import ch.ralena.aeonlineschedule.adapters.StudentSelectAdapter;
 import ch.ralena.aeonlineschedule.objects.Student;
+import io.realm.Case;
 import io.realm.Realm;
 
 public class StudentSelectFragment extends Fragment {
 	Realm realm;
 	List<Student> students;
+	EditText studentEdit;
+	StudentSelectAdapter adapter;
 
 	@Nullable
 	@Override
@@ -28,13 +35,46 @@ public class StudentSelectFragment extends Fragment {
 		students = realm.where(Student.class).findAllSorted("name");
 		View view = inflater.inflate(R.layout.fragment_student_select, container, false);
 
+		studentEdit = view.findViewById(R.id.studentNameEdit);
+		studentEdit.addTextChangedListener(new TextWatcher() {
+			@Override
+			public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+			}
+
+			@Override
+			public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+				adapter.updateStudents(realm.where(Student.class).contains("name", charSequence.toString(), Case.INSENSITIVE).findAllSorted("name"));
+			}
+
+			@Override
+			public void afterTextChanged(Editable editable) {
+			}
+		});
+
+		// set up recyclerview
 		RecyclerView recyclerView = view.findViewById(R.id.recyclerView);
-		StudentSelectAdapter adapter = new StudentSelectAdapter(students);
+		adapter = new StudentSelectAdapter(students);
 		recyclerView.setAdapter(adapter);
 		LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
 		recyclerView.setLayoutManager(layoutManager);
 		DividerItemDecoration itemDecoration = new DividerItemDecoration(getContext(), layoutManager.getOrientation());
 		recyclerView.addItemDecoration(itemDecoration);
+
+		// set up rxjava subscriptions to clicks
+		adapter.asObservableStudent().subscribe(student -> {
+			Toast.makeText(getContext(), student.getName(), Toast.LENGTH_SHORT).show();
+		});
+		adapter.asObservableButton().subscribe(v -> {
+			String name = studentEdit.getText().toString().trim();
+			if (name.isEmpty()) {
+				Toast.makeText(getContext(), "Student name must not be empty!", Toast.LENGTH_SHORT).show();
+			} else {
+				Toast.makeText(getContext(), "Creating student!", Toast.LENGTH_SHORT).show();
+			}
+		});
+
+
 		return view;
 	}
 }
